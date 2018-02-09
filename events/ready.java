8da1ca1951d2b8e98917d46dcd1ecc27a7cdf44e
InsectLawyer <36277366+InsectLawyer@users.noreply.github.com>
2018-02-09 10:27:10 -0800

    Attempt to make timezone work properly
    Set timezone to LA

--- events/ready.java
+++ events/ready.java
@@ -4,7 +4,7 @@ exports.run = (client) => {
    var channel = client.channels.get("187700521912500225");
    var interval = setInterval (function() {
       var date = new Date();
-      var time = date.toLocaleTimeString('en-US', { hour12: false, hour: "numeric", minute: "numeric"});
+      var time = date.toLocaleTimeString('en-US', 'America/Los_Angeles', { hour12: false, hour: "numeric", minute: "numeric"});
       //console.log(`Checked time at :`, time);
       if (time  === "16:20") {
          channel.send("cod is life").catch(console.error);
