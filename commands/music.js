2653ad90f14658feafd3fa493011dfb3c8307c68
InsectLawyer <36277366+InsectLawyer@users.noreply.github.com>
2018-02-14 22:25:50 -0800

    Music!
    Added functionality for one song. !music Bow

--- commands/music.js
+++ commands/music.js
@@ -1,10 +1,13 @@
 exports.run = (client, message, args) => {
 //const ytdl = require('..');
+const fs = require('fs');
+var filename = './music/' + args[0] + '.wav';
+console.log(filename);
  if (message.member.voiceChannel) {
    message.member.voiceChannel.join()
      .then(connection => { // Connection is an instance of VoiceConnection
        message.reply('I have successfully connected to the channel!');
-       var dispatcher = connection.playFile('C:/Users/Joe/Desktop/HMB/Bow.wav');
+       var dispatcher = connection.playFile(filename);
         //const url = 'https://www.youtube.com/watch?v=wAu_fYHZKLs&list=LLcwy7e-9ADZK7BMxtd8B5AA&index=7';
         //const stream = ytdl('https://www.youtube.com/watch?v=wAu_fYHZKLs&list=LLcwy7e-9ADZK7BMxtd8B5AA&index=7', { filter: 'audioonly' });
         //const dispatcher = connection.playStream(stream);
@@ -15,6 +18,7 @@ exports.run = (client, message, args) => {
       dispatcher.on('error', e => {
         // Catch any errors that may arise
         console.log(e);
+        message.reply('Bad input :(');
       });
      })
      .catch(console.log);
