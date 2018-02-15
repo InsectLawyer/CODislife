e2779ed8d21a44115f27493adde83be516d4c449
InsectLawyer <36277366+InsectLawyer@users.noreply.github.com>
2018-02-14 18:06:18 -0800

    Commands fixed
    !commands command will now work, Also small adjustment to add command

--- commands/commands.js
+++ commands/commands.js
@@ -1,13 +1,5 @@
 exports.run = (client, message, args) => {
-   const readline = require('readline');
    const fs = require('fs');
-   var lineReader = require('readline').createInterface({
-      input: fs.createReadStream('..\\cod\\commands\\commands.txt')
-   });
-   var response = [];
-   lineReader.on('line', function (line) {
-         response.push('!' + line);
-   });
-   console.log('response', response);
-   message.channel.send(response.toString()).catch(console.error);
+   var files = fs.readdirSync('./commands');
+   message.channel.send(files);
 }
