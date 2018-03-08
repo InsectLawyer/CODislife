cdfbe1a0b9d7671d2883bf66cac191fe195c8dca
InsectLawyer <36277366+InsectLawyer@users.noreply.github.com>
2018-03-07 20:37:51 -0800

    Test
    test console log

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -2,6 +2,7 @@ exports.run = (client, oldMember, newMember) => {
    // If someone is playing CSGO and enters a voice channel, Assembles crew
    var channel = client.channels.get("187700521912500225");
    if (newMember.voiceChannel && !newMember.voiceChannel.full) {
+      console.log(newMember.voiceChannel);
       if (newMember.presence.game && newMember.presence.game.name === "Counter-Strike Global Offensive") {
             channel.send('@everyone Assemble the Crew').catch(console.error);
       }
