d9146791d00976a723728943de18a80dbdfbb24e
InsectLawyer <36277366+InsectLawyer@users.noreply.github.com>
2018-11-05 15:35:19 -0800

    DST fix
    Now runs at 4:20 again

--- events/ready.java
+++ events/ready.java
@@ -6,8 +6,9 @@ exports.run = (client) => {
       var date = new Date();
       var time = date.toLocaleTimeString('en-US', { hour12: false, hour: "numeric", minute: "numeric"});
       //console.log(`Checked time at :`, time);
-      if (time  === "23:20") {
+      if (time  === "00:20") {
          // daylight time  === "00:20"
+         // else === "23:20"
          channel.send("cod is life").catch(console.error);
       }
    }, 60000);
