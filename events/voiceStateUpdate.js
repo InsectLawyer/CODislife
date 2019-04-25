fce1caa98125f4eac53009c5bf6bef88d286bf81
InsectLawyer <36277366+InsectLawyer@users.noreply.github.com>
2019-04-24 22:26:40 -0700

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,15 +1,16 @@
 exports.run = (client, oldMember, newMember) => {
    // If someone is playing CSGO and enters a voice channel, Assembles crew
-   var channel = client.channels.get("187700521912500225");
-   if (newMember.voiceChannel && !newMember.voiceChannel.full) {
-      console.log(newMember.voiceChannel);
-      var ppl = channel.members.size;
-      if (newMember.presence.game && newMember.presence.game.name === "Counter-Strike Global Offensive") {
-         var mes = '@everyone Assemble the Crew. ';
-         var limit = newMember.voiceChannel.userLimit;
-         if (limit > 0) {
-            mes += ppl + "/" + limit + " assembled";
-         }
+   var channel = client.channels.get("226492992242843649");
+   if (newMember.voiceChannel) {
+      //console.log(newMember.voiceChannel);
+      //var ppl = channel.members.size;
+      //if (newMember.presence.game && newMember.presence.game.name === "Counter-Strike Global Offensive") {
+         //var mes = '@everyone Assemble the Crew. ';
+         //var limit = newMember.voiceChannel.userLimit;
+         //if (limit > 0) {
+         //   mes += ppl + "/" + limit + " assembled";
+         //}
+         var mes = '@everyone THERE IS A SITUATION ENTER THE SITUATION ROOM STAT";
          channel.send(mes).catch(console.error);
       }
    }
