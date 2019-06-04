bda2ce6d268da8828bf10d5c0c2e0a7019d82b98
ubelhj <ubelhj@uw.edu>
2019-06-04 10:59:18 -0700

    more meme theft

--- commands/meme.js
+++ commands/meme.js
@@ -12,21 +12,20 @@ exports.run = (client, message, args) => {
         toRead = attach[0].url;
     }
     Jimp.read(toRead).then(function (image) {
-        let clone = image.clone();
-        clone.autocrop();
+        image.autocrop();
         let filename = "MyMeme" + getRandomInt(1000) + ".png";
-        let height = clone.bitmap.height;
-        let width = clone.bitmap.width;
+        let height = image.bitmap.height;
+        let width = image.bitmap.width;
 
-        clone.resize(width, height + 300);
+        image.resize(width, height + 300);
 
         Jimp.loadFont("https://fonts.googleapis.com/css?family=Sigmar+One&display=swap").then(font => {
             // load font from .fnt file
-            image.print(font, 0, 0, "TEST MESSAGE")
+            image.print(font, 0, 0, "TEST MESSAGE");
         });
 
 
-        clone.getBuffer(Jimp.MIME_PNG, function(err, buff) {
+        image.getBuffer(Jimp.MIME_PNG, function(err, buff) {
             if (err) {
                 return;
             } else {
