987b0af51772d4ceb4140f3c18326c8bf4fafbe0
InsectLawyer <36277366+InsectLawyer@users.noreply.github.com>
2019-06-04 11:41:14 -0700

    LAST CHANCE

--- commands/meme.js
+++ commands/meme.js
@@ -6,39 +6,26 @@ exports.run = (client, message, args) => {
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
     }
-
+    
     let attach = message.attachments.array();
     let toRead = args[0];
     if (attach[0] != null) {
         toRead = attach[0].url;
     }
     Jimp.read(toRead).then(function (image) {
-        image.autocrop();
+        let clone = image.clone();
         let filename = "MyMeme" + getRandomInt(1000) + ".png";
-        let height = image.bitmap.height;
-        let width = image.bitmap.width;
+        //let height = image.bitmap.height;
+        //let width = image.bitmap.width;
 
-        image.resize(width, height + 300);
-        console.log(Jimp.FONT_SANS_64_WHITE);
+        clone.resize(width, height + 300);
 
         Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(font => {
-            image.print(font, 10, 10, "TEST MESSAGE");
-            // image.print(
-            //     font,
-            //     0,
-            //     0,
-            //     {
-            //         text: "TEST MESSAGE",
-            //         alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-            //         alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
-            //     }
-                // maxWidth,
-                // maxHeight
-            // ); // prints 'Hello world!' on an image, middle and center-aligned, when x = 0 and y = 0
+            clone.print(font, 10, 10, "TEST MESSAGE");
         });
 
 
-        image.getBuffer(Jimp.MIME_PNG, function(err, buff) {
+        clone.getBuffer(Jimp.MIME_PNG, function(err, buff) {
             if (err) {
                 return;
             } else {
