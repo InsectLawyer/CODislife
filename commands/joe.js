e966a2b600d7cd2625f884b8a70c49aa41ea61e3
InsectLawyer <36277366+InsectLawyer@users.noreply.github.com>
2019-07-03 13:30:10 -0700

    Joe! takes the stage

--- commands/joe.js
+++ commands/joe.js
@@ -1,3 +1,44 @@
 exports.run = (client, message, args) => {
-    message.channel.send("Joe? Joe Ubelhart? That guy doesn't edge test his code").catch(console.error);
+   var Jimp = require("jimp");
+   var Attachment = require('discord.js').Attachment;
+   
+   function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+   }
+   
+   Jimp.read("./img/bidenjeb.png").then(function (joe) {
+      var attach = message.attachments.array();
+      var toRead = args[0];
+      if (attach[0] != null) {
+         toRead = attach[0].url;
+      }
+      Jimp.read(toRead).then(function (image) {
+         var clone = image.clone();
+         var filename = "image" + getRandomInt(1000) + ".png";
+         var height = image.bitmap.height;
+         var width = image.bitmap.width;
+         joe.resize(width, Jimp.AUTO);
+         height -= joe.bitmap.height;
+         width = (width / 2) - (joe.bitmap.width / 2);
+         if (height < 0) {
+            height = 0;
+         } 
+         if (width < 0) {
+            width = 0;
+         }
+         clone.composite(joe, width, height);
+         clone.getBuffer(Jimp.MIME_PNG, function(err, buff) {
+            if (err) {
+               return;
+            } else {
+               message.channel.send(new Attachment(buff, filename)).catch(console.error);
+            }
+         });
+         message.delete().catch(console.error);
+      }).catch(function (err) {
+         console.log(err);
+      });
+   }).catch(function (err) {
+      console.log(err);
+   });
 }
